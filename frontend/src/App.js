96a3bafe84fa7c5e2b16fa081aa9da66980015c5
harikamekala222 <harikamekala222@gmail.com>
2026-07-28 20:12:49 +0530

    Update API URL to new backend address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 // Backend URL
-const API_URL = "http://ec2-ip:5000";
+const API_URL = "http://13.232.163.12:5000";
 
 function App() {
   const [contacts, setContacts] = useState([]);
